2b5cf0677ef6f87b71b96e85a84d2e1ee5b37b46
daoson12 <daoduvictor48@gmail.com>
2022-09-23 22:19:41 +0000

    created hostel detail and hotel-list

---
+++ src/app/components/hotel/hotel-detail/hotel-detail.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { HotelDetailComponent } from './hotel-detail.component';
+
+describe('HotelDetailComponent', () => {
+  let component: HotelDetailComponent;
+  let fixture: ComponentFixture<HotelDetailComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HotelDetailComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HotelDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/hotel/hotel-list/hotel-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-list',
+  templateUrl: './hotel-list.component.html',
+  styleUrls: ['./hotel-list.component.css']
+})
+export class HotelListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
