591d7dc83542a370ab16e001e1891d558ace566c
daoson12 <daoduvictor48@gmail.com>
2022-09-23 19:13:47 +0000

    add images and modified landing page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { LandingPageComponent } from './layouts/landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LandingPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
