f13ad9ca274fa408ffc6a41c726556a3435e277d
daoson12 <daoduvictor48@gmail.com>
2022-09-23 22:23:25 +0000

    added hostel-list route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { HotelListComponent } from './components/hotel/hotel-list/hotel-list.component';
 import { LandingPageComponent } from './layouts/landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', component:LandingPageComponent}
+  {path:'', component:LandingPageComponent},
+  {path:'hotel-list', component:HotelListComponent}
 ];
 
 @NgModule({
