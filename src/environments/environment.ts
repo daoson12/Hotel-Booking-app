08a098d1bc1b6fa79409f8fcca2f50ea602b9d46
daoson12 <daoduvictor48@gmail.com>
2022-09-29 18:46:13 +0000

    added backend swagger link on environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl:'http://192.168.152.9:9000/swagger/swagger-ui/index.html'
 };
 
 /*
